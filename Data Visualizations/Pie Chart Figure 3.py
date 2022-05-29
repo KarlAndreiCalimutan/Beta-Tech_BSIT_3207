8db4f0baf3781eea8225749175710e0df0b26ba6
Karl Andrei G. Calimutan <karlandrei.calimutan@g.batstate-u.edu.ph>
2022-05-29 20:15:07 +0800

    updating visuals

--- Data Visualizations/Pie Chart Figure 3.py
+++ Data Visualizations/Pie Chart Figure 3.py
@@ -5,13 +5,12 @@ share = [14, 10,0]
 
 explode = (0.1,0,0)
 plt.style.use('ggplot')
-plt.title('20 years Top 1 Country Floodsubtype ')
+plt.title('Thailand flood Subtype in 20 years')
 plt.pie(x=share, explode=explode, labels=labels, autopct='%.2f%%',
         startangle=90)
 plt.axis('equal')
 plt.legend(loc='upper right')
 
 
-#circle = plt.Circle(xy=(0,0), radius=.75, facecolor='white')
-#plt.gca().add_artist(circle)
+
 plt.show()
